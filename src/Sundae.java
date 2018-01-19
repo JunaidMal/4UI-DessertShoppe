2385e88d928f9e648d1552d8be07cb70788f151c
malij6756 <malij6756@HRH-IC0027263.wrdsb.ca>
2018-01-19 14:21:16 -0500

    done

--- src/Sundae.java
+++ src/Sundae.java
@@ -6,22 +6,45 @@
 
 /**
  *
- * @author CHANGE_THIS_TO_YOUR_NAME
+ * @author malij6756
  */
-public class Sundae{
-    
+public class Sundae extends DessertItem{
+    // private variables for name, cost, topping name and cost
+    private String name;
+    private int icCost;
+    private String toppingName;
+    private int toppingCost;
 
-    
-    public Sundae(String icName, int icCost, String toppingName, int toppingCost)
-    {
-       
+    // updating name, cost, topping name and cost variables
+    public Sundae(String icName, int icCost, String toppingName, int toppingCost) {
+       this.name = icName;
+       this.icCost = icCost;
+       this.toppingName = toppingName;
+       this.toppingCost = toppingCost;
     }
     
 
-    
+    @Override
     public String toString()
     {
+        // printing name of topping with sundae name on a new line
+        String printSundae = toppingName + " Sundae with \n";
+        printSundae += name;
+        // converting total cost from cents to dollars
+        String holder = DessertShoppe.cents2dollarsAndCents(getCost());
+        // creating spaces between name and cost in reference to receipt width
+        for (int i = 0; i < DessertShoppe.RECEIPT_WIDTH - holder.length() - name.length(); i++) {
+            printSundae += " ";
+        }
         
+        // adding cost to the string then returning it
+        printSundae += holder;
+        return printSundae;
+    }
+    
+    public int getCost() {
+        // adding topping cost to the cost of the sundae
+        return toppingCost + icCost;
     }
     
 }
